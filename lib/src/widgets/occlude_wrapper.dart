b07c73bb90b215429e65c060268c223d8c4790c8
Nsikak Isaac <nsikak@tradearies.com>
2024-11-07 05:35:04 -0800

    fix: start ticker

--- lib/src/widgets/occlude_wrapper.dart
+++ lib/src/widgets/occlude_wrapper.dart
@@ -31,7 +31,7 @@ class _OccludeWrapperState extends State<OccludeWrapper>
       if (enableOcclusion) {
         getOccludePoints();
       }
-    });
+    })..start();
     super.initState();
   }
 
